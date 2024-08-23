e669e2304ed605cada80fefe9e7e9d489ae26ad1
NateMerritt <nate.merritt@biblionexus.org>
2024-08-23 16:04:50 -0400

    Add project files.

---
+++ AvaloniaPrototype/AvaloniaPrototype/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaPrototype.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
